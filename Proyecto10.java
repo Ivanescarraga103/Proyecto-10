114f891d41eab4a4365f1e0fd5717f2c646a714c
Ivanescarraga103 <elproivan002@gmail.com>
2023-09-24 22:20:49 -0500

    primer commit

---
+++ Proyecto10.java
@@ -0,0 +1,15 @@
+package com.mycompany.Ivan;
+import java.util.Scanner;
+public class Proyecto10 {
+public static void main(String[] args) {
+ Scanner leer= new Scanner (System.in);
+ float num1,num2,res;
+ System.out.print("Ingresa el primer numero");
+ num1=leer.nextFloat();
+ System.out.print("Ingresa el segundo numero");
+ num2=leer.nextFloat();
+ res=num1+num2;
+ System.out.print("La suma es :"+res);
+
+    }
+}
